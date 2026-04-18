807216aa3c91708e421454045920db7c680ae739
Sahithi Karise <ksahithi9615@gmail.com>
2026-04-18 13:34:29 +0530

    Uploading all the internship tasks

---
+++ Cybersecurity/Task-2/keycodes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  1: 'KEY_ESC',
+  16: 'KEY_Q',
+  17: 'KEY_W',
+  18: 'KEY_E',
+  19: 'KEY_R',
+  30: 'KEY_A',
+  31: 'KEY_S',
+  32: 'KEY_D',
+  33: 'KEY_F',
+  57: 'KEY_SPACE',
+  28: 'KEY_ENTER',
+  14: 'KEY_BACKSPACE',
+  240: 'KEY_UNKNOWN'
+};
